fe669b9e1e7568766ec2a82d674521568d0345d1
velganeshchandra <velganeshchandra@gmail.com>
2020-12-22 21:04:15 +0800

    first commit

---
+++ inputage.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//this is for storing numbers
+int main(){
+	
+    int age; //this makes a vairable for you to store the value in
+    printf("enter in your age: "); //this prints a prompt for the user
+    scanf("%d", &age); //this takes in the argument given and stores it
+                       //into the address of age
+
+	return 0;
+}
