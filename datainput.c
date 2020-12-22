fdd78b25d1c9b37ed2a390a41dfd4711b02cd8c3
velganeshchandra <velganeshchandra@gmail.com>
2020-12-22 20:57:02 +0800

    first commit

---
+++ datainput.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//this is for entering and storing strings
+int main(){
+    char name[20];
+
+    printf("Enter a name : ");
+    scanf("%s", &name);
+
+    printf("the name entered is: %s\n", name);
+
+
+
+return 0;
+}
